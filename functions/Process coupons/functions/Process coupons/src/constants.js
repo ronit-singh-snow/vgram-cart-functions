4a860b5907dab79f476e5ef73d1f5d082e25be2c
ronit-singh-snow <ronit.singh.snow@gmail.com>
2025-01-08 10:05:59 +0530

    Removing extra files

--- functions/Process coupons/functions/Process coupons/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const PROJECT_ID = "66efac960005a73d7247";
-export const APPWRITE_ENDPOINT = "https://cloud.appwrite.io/v1";
-export const DB_NAME = "orange_cart_db";
-export const COUPON_COLLECTION_ID = "674170d3001ddf8a2830";
-export const ORDER_COLLECTION_ID = "67286a0e0016933ec4b1";
-export const API_KEY = "standard_ac36ebd531aaa7154e2b728793ce42400bb62224cfd91927d418915b4142d4f008b417c8f4b97d26e7e3dfda3569c5d24a583e4dd41c3f0df07e9eb512adc8c84132f498abd5629b25008a918b18e0c0811231e6d52df4b5bab4a1b2e42f2fc59fac6e94ba5af1c6e8994c7030567e0712d1472bf759b4d59b861126adcff612";
